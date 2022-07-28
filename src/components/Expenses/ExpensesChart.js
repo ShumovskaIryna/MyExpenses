f55db6977d9fefb9700c69779dcca39cf5bebdc4
Кіт Frontend-щик <71092957+ShumovskaIryna@users.noreply.github.com>
2022-07-29 02:44:19 +0300

    first commits

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chart from '../Chart/Chart';
+
+// import './ExpensesChart.css';
+
+const ExpensesChart = (props) => {
+
+  return (
+      <div className='chart-bar__label'>
+          <Chart />
+      </div>
+  );
+};
+
+export default ExpensesChart;
